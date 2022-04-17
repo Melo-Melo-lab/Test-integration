8ed2e19956c4d14f87d97807a243cf247c46a289
melo <lopmelodie@gmail.com>
2022-04-17 17:05:47 +0200

    v1.2

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
 // JavaScript Document
-$(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        autoWidth:true,
-        loop:true,
-        onSliderLoad: function() {
-            $('#autoWidth').removeClass('cS-hidden');
-        }
-    });
+$(document).ready(function () {
+  $('#autoWidth').lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $('#autoWidth').removeClass('cS-hidden');
+    }
   });
+});
+
+$(document).ready(function () {
+  $('#autoWidth2').lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $('#autoWidth2').removeClass('cS-hidden2');
+    }
+  });
+});
 
 
-  function lowAgeBtnClick() {
-    //window.scrollTo({top:300,behavior: 'smooth'});
-    
-    var element = document.getElementById("lowage");
+function lowAgeBtnClick() {
+  //window.scrollTo({top:300,behavior: 'smooth'});
 
-    element.scrollIntoView();
-    element.scrollIntoView(true);
-    element.scrollIntoView({block: "start"});
-    element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  var element = document.getElementById("lowage");
 
-  }
+  element.scrollIntoView();
+  element.scrollIntoView(true);
+  element.scrollIntoView({ block: "start" });
+  element.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+
+}
